7cfeca66ffacc208df6298a7eacbc9cbba6781e2
adam klein <adamklein.mail@gmail.com>
2013-10-02 15:24:09 +0200

    Allowing email not to be unique

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -27,7 +27,7 @@ module Devise
 
         base.class_eval do
           validates_presence_of   :email, :if => :email_required?
-          validates_uniqueness_of :email, :allow_blank => true, :if => :email_changed?
+          validates_uniqueness_of :email, :allow_blank => true, :if => 'email_changed? && email_unique_required?'
           validates_format_of     :email, :with  => email_regexp, :allow_blank => true, :if => :email_changed?
 
           validates_presence_of     :password, :if => :password_required?
@@ -55,6 +55,10 @@ module Devise
       end
 
       def email_required?
+        true
+      end
+
+      def email_unique_required?
         true
       end
 
